935c43f1093f12ffe4442faac5eb324834ecf0ae
Breno <brenluz@outlook.com>
2024-07-22 11:22:49 -0300

    fix: 429 error nao acontece mais

--- src/update.py
+++ src/update.py
@@ -7,9 +7,9 @@ def addInitialValues(sheet1, sheet2):
     coluna1 = sheet1.col_values(1)
     coluna2 = sheet1.col_values(2)
     if isEmpty(sheet2.col_values(1)):
-        writeToCol(sheet2, 1, coluna1)
+        updateBatch(sheet2, 1, coluna1)
     if isEmpty(sheet2.col_values(2)):
-        writeToCol(sheet2, 2, coluna2)
+        updateBatch(sheet2, 2, coluna2)
 
 def isEmpty(array):
     string = array
